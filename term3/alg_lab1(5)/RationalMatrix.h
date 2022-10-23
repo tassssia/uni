21dc29d407bca6c189fff65a7f7ab2159787ae2d
tassssia <113410135+tassssia@users.noreply.github.com>
2022-10-23 23:44:55 +0300

    Update RationalMatrix.h

--- term3/alg_lab1(5)/RationalMatrix.h
+++ term3/alg_lab1(5)/RationalMatrix.h
@@ -7,6 +7,13 @@ class RationalMatrix {
 	int rows;
 	int cols;
 	std::vector<std::vector<fract>> content;
+
+	// for Strassen's multiplication
+	void extend(int maxSize);
+	RationalMatrix multMin(const RationalMatrix* b, int ia, int ja, int ib, int jb);
+	RationalMatrix multRec(RationalMatrix* b, int ia, int ja, int ib, int jb, int size);
+	void shrink(int height, int width);
+
 public:
 	void print() {
 		for (int i = 0; i < this->rows; i++)
@@ -28,10 +35,13 @@ public:
 		for (int i = 0; i < height; i++)
 		{
 			for (int j = 0; j < width; j++)
-				tmp.push_back((this->content)[ia + i][ja + j].sum(b->content[ib + i][jb + j]));
+				tmp.push_back((this->content)[ia + i][ja + j].sum((b->content)[ib + i][jb + j]));
 			res.content.push_back(tmp);
 			tmp = {};
 		}
+
+		res.rows = height;
+		res.cols = width;
 
 		return res;
 	}
@@ -51,10 +61,13 @@ public:
 		for (int i = 0; i < height; i++)
 		{
 			for (int j = 0; j < width; j++)
-				tmp.push_back((this->content)[ia + i][ja + j].diff(b->content[ib + i][jb + j]));
+				tmp.push_back((this->content)[ia + i][ja + j].diff((b->content)[ib + i][jb + j]));
 			res.content.push_back(tmp);
 			tmp = {};
 		}
+
+		res.rows = height;
+		res.cols = width;
 
 		return res;
 	}
@@ -64,6 +77,8 @@ public:
 		if (this->cols != b->cols || this->rows != b->rows) return res;
 		return this->diff(b, 0, 0, 0, 0, this->rows, this->cols);
 	}
+
+	RationalMatrix StrassensMult(RationalMatrix b);
 
 	RationalMatrix(std::vector<std::vector<fract>> matrix = { {} }) {
 		content = matrix;
