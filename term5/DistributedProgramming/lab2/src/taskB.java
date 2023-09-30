59b0b4478cb09896d4ee2d77f9b0b9401973c6a6
tassssia <113410135+tassssia@users.noreply.github.com>
2023-09-30 20:36:35 +0300

    Create taskB.java

---
+++ term5/DistributedProgramming/lab2/src/taskB.java
@@ -0,0 +1,114 @@
+import java.security.SecureRandom;
+import java.util.Stack;
+import java.util.concurrent.SynchronousQueue;
+
+class PieceOfProperty {
+    private int id;
+    private int cost;
+
+    public PieceOfProperty (int id, int cost) {
+        this.id = id;
+        this.cost = cost;
+    }
+
+
+    public int getId() {
+        return id;
+    }
+    public int getCost() {
+        return cost;
+    }
+}
+
+public class taskB {
+    private SecureRandom random = new SecureRandom();
+    //private int statePropertyNum;
+    private final Stack<PieceOfProperty> stateProperty = new Stack<PieceOfProperty>();
+    private static int minCost = 10;
+    private static int maxCost = 200;
+    private int totalNum = 0;
+    private SynchronousQueue<PieceOfProperty> toLoad = new SynchronousQueue<PieceOfProperty>();
+    private SynchronousQueue<PieceOfProperty> toCount = new SynchronousQueue<PieceOfProperty>();
+
+    public taskB(int statePropertyNum) {
+        //this.statePropertyNum = statePropertyNum;
+        for (int i = 0; i < statePropertyNum; i++) {
+            stateProperty.push(new PieceOfProperty(i, random.nextInt(minCost + maxCost) - minCost)) ;
+        }
+        totalNum = statePropertyNum;
+    }
+
+    public void IvanovAct() throws InterruptedException {
+        PieceOfProperty current;
+        int taken = 0;
+        while (taken != totalNum) {
+            current = stateProperty.pop();
+            System.out.println("Ivanov has taken item " + current.getId());
+            this.toLoad.put(current);
+            taken++;
+
+            //Thread.sleep(400);
+        }
+    }
+    public void PetrovAct() throws InterruptedException {
+        PieceOfProperty current;
+        int loaded = 0;
+        while (loaded != totalNum) {
+            current = this.toLoad.take();
+            System.out.println("Petrov is loading item " + current.getId());
+            this.toCount.put(current);
+            loaded++;
+
+            //Thread.sleep(500);
+        }
+    }
+    public int NechyporchukAct() throws InterruptedException {
+        PieceOfProperty current;
+        int totalCost = 0;
+        int counted = 0;
+        while (counted != totalNum) {
+            current = this.toCount.take();
+            System.out.println("Nechyporchuk has added cost of item " + current.getId());
+            totalCost += current.getCost();
+            counted++;
+
+            //Thread.sleep(1200);
+        }
+
+        return totalCost;
+    }
+
+    public static void main(String[] args) throws InterruptedException {
+        taskB b = new taskB(10);
+
+        Thread Ivanov = new Thread(() -> {
+            try {
+                b.IvanovAct();
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+        });
+        Thread Petrov = new Thread(() -> {
+            try {
+                b.PetrovAct();
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+        });
+        Thread Nechyporchuk = new Thread(() -> {
+            try {
+                b.NechyporchukAct();
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+        });
+
+        Ivanov.start();
+        Petrov.start();
+        Nechyporchuk.start();
+
+        Ivanov.join();
+        Petrov.join();
+        Nechyporchuk.join();
+    }
+}
