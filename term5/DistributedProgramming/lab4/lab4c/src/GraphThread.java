807faa23607c1d181e5341c6c1578945a4044d82
tassssia <113410135+tassssia@users.noreply.github.com>
2023-10-15 13:45:46 +0300

    lab 4c

---
+++ term5/DistributedProgramming/lab4/lab4c/src/GraphThread.java
@@ -0,0 +1,138 @@
+import java.util.Random;
+import java.security.SecureRandom;
+public abstract class GraphThread extends Thread {
+    Graph graph;
+}
+
+class PriceChanger extends GraphThread {
+    public PriceChanger(Graph graph) {
+        this.graph = graph;
+    }
+
+    @Override
+    public void run() {
+        SecureRandom random = new SecureRandom();
+        while (true) {
+            int i = random.nextInt(graph.getSize());
+            int j = random.nextInt(graph.getSize());
+            int newPrice = random.nextInt(100) + 1;
+
+            try {
+                while (graph.getCost(i, j) == 0) {
+                    i = random.nextInt(graph.getSize());
+                    j = random.nextInt(graph.getSize());
+                    newPrice = random.nextInt(100) + 1;
+                }
+            }
+            catch (Exception e) {
+                e.printStackTrace();
+            }
+
+            graph.LockWrite(i, j);
+            System.out.println("Changing cost of " + i + " - " + j);
+            try {
+                graph.setCost(i, j, newPrice);
+                System.out.println("Set cost of " + i + " - " + j + " to " + newPrice);
+            }
+            catch (Exception e) {
+                e.printStackTrace();
+            }
+            System.out.println("Finished changing costs");
+            graph.UnlockWrite(i, j);
+
+            try {
+                Thread.sleep(5000);
+            }
+            catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+        }
+    }
+}
+
+class WaysEditor extends GraphThread {
+    public WaysEditor(Graph graph) {
+        this.graph = graph;
+    }
+
+    @Override
+    public void run() {
+        SecureRandom random = new SecureRandom();
+        while (true) {
+            int i = random.nextInt(graph.getSize());
+            int j = random.nextInt(graph.getSize());
+
+            graph.LockWrite(i, j);
+            System.out.println("Editing way " + i + " - " + j);
+            try {
+                if (graph.getCost(i, j) == 0) {
+                    graph.setCost(i, j, random.nextInt(graph.getSize()));
+                    System.out.println("Way " + i + " - " + j + " was added with a cost of " + graph.getCost(i, j));
+                }
+                else {
+                    graph.setCost(i, j, 0);
+                    System.out.println("Way " + i + " - " + j + " was removed");
+                }
+            }
+            catch (Exception e) {
+                e.printStackTrace();
+            }
+            System.out.println("Finished editing ways");
+            graph.UnlockWrite(i, j);
+
+            try {
+                Thread.sleep(5000);
+            }
+            catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+        }
+    }
+}
+
+class CitiesEditor extends GraphThread {
+    public CitiesEditor(Graph graph) {
+        this.graph = graph;
+    }
+
+    @Override
+    public void run() {
+        SecureRandom random = new SecureRandom();
+        int city = random.nextInt(graph.getSize() * 2);
+
+        graph.LockWrite();
+        System.out.println("Editing cities");
+        try {
+            if (city < graph.getSize()) {
+                graph.removeCity(city);
+                System.out.println("The city " + city + " was removed");
+            } else {
+                graph.addCity();
+                System.out.println("One more city was added, total number of them is " + graph.getSize() + "now");
+            }
+        }
+        catch (Exception e) {
+            e.printStackTrace();
+        }
+        System.out.println("Finished editing cities");
+        graph.UnlockWrite();
+
+        try {
+            Thread.sleep(5000);
+        }
+        catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+    }
+}
+
+class WaySearcher extends GraphThread {
+    public WaySearcher(Graph graph) {
+        this.graph = graph;
+    }
+
+    @Override
+    public void run() {
+
+    }
+}
--- term5/DistributedProgramming/lab4/lab4c/src/taskC.java
+++ term5/DistributedProgramming/lab4/lab4c/src/taskC.java
@@ -1,29 +1,30 @@
 public class taskC {
     public static void main(String[] args) {
-        Graph matrix = new Graph(5);
-        WorkThread workThread1 = new WorkThread(matrix, 1);
-        WorkThread workThread2 = new WorkThread(matrix, 2);
-        WorkThread workThread3 = new WorkThread(matrix, 3);
-        WorkThread workThread4 = new WorkThread(matrix, 4);
+        Graph routes = new Graph(5);
+        GraphThread thread1 = new PriceChanger(routes);
+        GraphThread thread2 = new WaysEditor(routes);
+        GraphThread thread3 = new CitiesEditor(routes);
+        GraphThread thread4 = new WaySearcher(routes);
 
-        workThread1.setDaemon(true);
-        workThread2.setDaemon(true);
-        workThread3.setDaemon(true);
-        workThread4.setDaemon(true);
+        thread1.setDaemon(true);
+        thread2.setDaemon(true);
+        thread3.setDaemon(true);
+        thread4.setDaemon(true);
 
         try {
-            workThread4.start();
-            workThread1.start();
+            thread4.start();
+            thread1.start();
             Thread.sleep(500);
-            workThread2.start();
+            thread2.start();
             Thread.sleep(500);
-            workThread3.start();
+            thread3.start();
 
-            workThread1.join();
-            workThread2.join();
-            workThread3.join();
-            workThread4.join();
-        } catch (InterruptedException e) {
+            thread1.join();
+            thread2.join();
+            thread3.join();
+            thread4.join();
+        } 
+        catch (InterruptedException e) {
             e.printStackTrace();
         }
     }
