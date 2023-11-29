1dd1f07ab6c335619bc458b7856918b99cf34c88
tassssia <113410135+tassssia@users.noreply.github.com>
2023-11-29 23:43:13 +0200

    preparation for parsers

---
+++ term5/OOP/lab2/src/main/java/parser/Handler.java
@@ -0,0 +1,80 @@
+package parser;
+
+import prototype.Knife;
+import org.xml.sax.Attributes;
+import org.xml.sax.SAXException;
+import org.xml.sax.helpers.DefaultHandler;
+import prototype.KnifeAttributes;
+import prototype.Visual;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Handler extends DefaultHandler {
+    private List<Knife> knives = new ArrayList<>();
+    private String attrValue;
+
+    @Override
+    public void startDocument() throws SAXException {
+        knives = new ArrayList<>();
+    }
+    @Override
+    public void startElement(String url, String name, String attrName, Attributes attributes) {
+        switch(attrName){
+            case KnifeAttributes.KNIFE:
+                Knife knife = new Knife();
+                knives.add(knife);
+                break;
+            case KnifeAttributes.VISUAL:
+                Visual visual = new Visual();
+                getLast().setVisual(visual);
+                break;
+        }
+    }
+    @Override
+    public void endElement(String uri, String name, String attrName) {
+        switch(attrName) {
+            case KnifeAttributes.ID:
+                getLast().setId(attrValue);
+                break;
+            case KnifeAttributes.TYPE:
+                getLast().setType(attrValue);
+                break;
+            case KnifeAttributes.HANDY:
+                getLast().setHandy(Integer.valueOf(attrValue));
+                break;
+            case KnifeAttributes.ORIGIN:
+                getLast().setOrigin(attrValue);
+                break;
+            case KnifeAttributes.BLADE_LENGTH_CM:
+                getLast().getVisual().setBladeLengthCm(Integer.valueOf(attrValue));
+                break;
+            case KnifeAttributes.BLADE_WIDTH_MM:
+                getLast().getVisual().setBladeWidthMm(Integer.valueOf(attrValue));
+                break;
+            case KnifeAttributes.MATERIAL:
+                getLast().getVisual().setMaterial(attrValue);
+                break;
+            case KnifeAttributes.HANDLE:
+                getLast().getVisual().setHandle(attrValue);
+                break;
+            case KnifeAttributes.BLOOD_GROOVE:
+                getLast().getVisual().setBloodGroove(Boolean.parseBoolean(attrValue));
+                break;
+            case KnifeAttributes.VALUE:
+                getLast().setValue(Boolean.parseBoolean(attrValue));
+                break;
+        }
+    }
+    @Override
+    public void characters(char[] ch, int start, int length) throws SAXException {
+        attrValue = new String(ch, start, length);
+    }
+
+    public List<Knife> getKnives() { 
+        return knives;
+    }
+    private Knife getLast() {
+        return knives.get(knives.size() - 1);
+    }
+}
---
+++ term5/OOP/lab2/src/main/java/prototype/KnifeAttributes.java
@@ -0,0 +1,16 @@
+package prototype;
+
+public interface KnifeAttributes {
+    static final String KNIFE = "Knife";
+    static final String ID = "id";
+    static final String TYPE = "type";
+    static final String HANDY = "handy";
+    static final String ORIGIN = "origin";
+    static final String VISUAL = "visual";
+    static final String BLADE_LENGTH_CM = "bladeLengthCm";
+    static final String BLADE_WIDTH_MM = "bladeWidthMm";
+    static final String MATERIAL = "material";
+    static final String HANDLE = "handle";
+    static final String BLOOD_GROOVE = "bloodGroove";
+    static final String VALUE = "value";
+}
